36aa429a89bf3e8eb8c1cdc98193d2eeda360cf1
John Stroy <hsplet+hg+323r@jdstroy.no-ip.com>
2012-01-08 17:35:01 -0500

    Forgot to add the CodeOpcodeCounter MethodAdapter to the repository.

---
+++ compiler/hsplet/compiler/CodeOpcodeCounter.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hsplet.compiler;
+
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodAdapter;
+import org.objectweb.asm.MethodVisitor;
+
+/**
+ *
+ * @author jdstroy
+ */
+public class CodeOpcodeCounter extends MethodAdapter {
+    private int count = 0;
+
+    public CodeOpcodeCounter(MethodVisitor mv) {
+        super(mv);
+    }
+
+    /* Gets the number of opcodes issued. */
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void visitFieldInsn(int opcode, String owner, String name, String desc) {
+        count++;
+        super.visitFieldInsn(opcode, owner, name, desc);
+    }
+
+    @Override
+    public void visitIincInsn(int var, int increment) {
+        count++;
+        super.visitIincInsn(var, increment);
+    }
+
+    @Override
+    public void visitInsn(int opcode) {
+        count++;
+        super.visitInsn(opcode);
+    }
+
+    @Override
+    public void visitIntInsn(int opcode, int operand) {
+        count++;
+        super.visitIntInsn(opcode, operand);
+    }
+
+    @Override
+    public void visitJumpInsn(int opcode, Label label) {
+        count++;
+        super.visitJumpInsn(opcode, label);
+    }
+
+    @Override
+    public void visitLdcInsn(Object cst) {
+        count++;
+        super.visitLdcInsn(cst);
+    }
+
+    @Override
+    public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
+        count++;
+        super.visitLookupSwitchInsn(dflt, keys, labels);
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String desc) {
+        count++;
+        super.visitMethodInsn(opcode, owner, name, desc);
+    }
+
+    @Override
+    public void visitMultiANewArrayInsn(String desc, int dims) {
+        count++;
+        super.visitMultiANewArrayInsn(desc, dims);
+    }
+
+    @Override
+    public void visitTableSwitchInsn(int min, int max, Label dflt, Label[] labels) {
+        count++;
+        super.visitTableSwitchInsn(min, max, dflt, labels);
+    }
+
+    @Override
+    public void visitTypeInsn(int opcode, String type) {
+        count++;
+        super.visitTypeInsn(opcode, type);
+    }
+
+    @Override
+    public void visitVarInsn(int opcode, int var) {
+        count++;
+        super.visitVarInsn(opcode, var);
+    }
+    
+}
