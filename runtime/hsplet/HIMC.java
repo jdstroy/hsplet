f2a2a6c95f81e3c58c342c9ba9dedc1d8b69de7f
John Stroy <hsplet+hg+323r@jdstroy.no-ip.com>
2012-05-29 21:03:52 -0400

    Adding HIMC and HWND

---
+++ runtime/hsplet/HIMC.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hsplet;
+
+/**
+ * This annotation decorates parameters which are handles to input contexts
+ * @author jdstroy
+ */
+public @interface HIMC {
+    
+}
---
+++ runtime/hsplet/HWND.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hsplet;
+
+/**
+ * A parameter decorated with this annotation is a handle to a window (HWND)
+ * @author jdstroy
+ */
+public @interface HWND {
+    
+}
