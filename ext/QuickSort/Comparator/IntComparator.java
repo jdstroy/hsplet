bc79235863bfd925ce2313354fb54c91e93dba92
John Stroy <hsplet+hg+323r@jdstroy.no-ip.com>
2012-06-24 02:30:07 -0400

    A little more typesafe in ext/QuickSort/Comparator/IntComparator.java, but still needing lots of work.

--- ext/QuickSort/Comparator/IntComparator.java
+++ ext/QuickSort/Comparator/IntComparator.java
@@ -2,8 +2,8 @@ package QuickSort.Comparator;
 
 import QuickSort.Container.*;
 
-public class IntComparator implements java.util.Comparator {
-	public int compare(final Object o1, final Object o2) {
+public class IntComparator implements java.util.Comparator<Container> {
+	public int compare(final Container o1, final Container o2) {
 		return ((IntContainer)o1).value - ((IntContainer)o2).value;
 	}
 }
