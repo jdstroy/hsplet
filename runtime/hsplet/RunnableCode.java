308b5c423a3c1e8b0266561773bf55fab5dee581
John Stroy <hsplet+hg+323r@jdstroy.no-ip.com>
2012-09-24 14:47:10 -0400

    Normalizing format for RunnableCode.java

--- runtime/hsplet/RunnableCode.java
+++ runtime/hsplet/RunnableCode.java
@@ -9,31 +9,27 @@ import hsplet.variable.Operand;
 import java.io.Serializable;
 
 /**
- * コンパイラによって*.axから生成される、実行可能なコードのクラスの基本クラス。
- * <p>
- * 実際のクラスはコンパイラによって生成されるまでわからないので、このインターフェイスを代わりに使用する。
- * </p>
- * 
+ * コンパイラによって*.axから生成される、実行可能なコードのクラスの基本クラス。 <p>
+ * 実際のクラスはコンパイラによって生成されるまでわからないので、このインターフェイスを代わりに使用する。 </p>
+ *
  * @author Yuki
  * @version $Revision: 1.2 $, $Date: 2006/05/09 11:57:31 $
  */
 public abstract class RunnableCode implements Runnable, Serializable {
 
-	public final void run() {
+    public final void run() {
 
-		try {
-			run(-1);
-		} catch (EndException e) {
+        try {
+            run(-1);
+        } catch (EndException e) {
+        }
+    }
 
-		}
-	}
-
-	/**
-	 * コードを実行する。
-	 * 
-	 * @param label 実行を開始するラベル。先頭から開始するときは -1。
-	 * @return コードの戻り値。戻り値が無いときは null。
-	 */
-	public abstract Operand run(final int label);
-
+    /**
+     * コードを実行する。
+     *
+     * @param label 実行を開始するラベル。先頭から開始するときは -1。
+     * @return コードの戻り値。戻り値が無いときは null。
+     */
+    public abstract Operand run(final int label);
 }
