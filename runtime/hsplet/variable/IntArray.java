12a7a77052ececec3592f7b72e43edec948e6256
John Stroy <hsplet+hg+323r@jdstroy.no-ip.com>
2012-10-13 18:01:17 -0400

    Delegate IntArray's assignment and retrieval operations to <code>void assignRaw(final int index, final int newValue)</code> and <code>int toIntRaw(final int index)</code>

--- runtime/hsplet/variable/IntArray.java
+++ runtime/hsplet/variable/IntArray.java
@@ -2,22 +2,29 @@
  * $Id: IntArray.java,v 1.2.4.1 2006/08/02 12:13:06 Yuki Exp $
  */
 package hsplet.variable;
+
 import hsplet.util.Conversion;
 
 /**
  * int の配列を表すクラス。
- * 
- * 
+ *
+ *
  * @author Yuki
  * @version $Revision: 1.2.4.1 $, $Date: 2006/08/02 12:13:06 $
  */
 public final class IntArray extends Array {
 
-    /** 直列化復元時に、データの互換性を確認するためのバージョン番号。 */
+    /**
+     * 直列化復元時に、データの互換性を確認するためのバージョン番号。
+     */
     private static final long serialVersionUID = -4406165790569093040L;
-    /** このクラスを含むソースファイルのバージョン文字列。 */
+    /**
+     * このクラスを含むソースファイルのバージョン文字列。
+     */
     private static final String fileVersionID = "$Id: IntArray.java,v 1.2.4.1 2006/08/02 12:13:06 Yuki Exp $";
-    /** オブジェクトの値。 */
+    /**
+     * オブジェクトの値。
+     */
     private int[] values;
 
     /**
@@ -29,10 +36,8 @@ public final class IntArray extends Array {
     }
 
     /**
-     * 要素数を指定してオブジェクトを構築する。
-     * <p>
-     * オリジナル HSP と違い、要素数に 0 を指定することは出来ない。 つまり、配列は必ず四次元あることになる。 そのほうが要素数などの計算が簡単。
-     * </p>
+     * 要素数を指定してオブジェクトを構築する。 <p> オリジナル HSP と違い、要素数に 0 を指定することは出来ない。
+     * つまり、配列は必ず四次元あることになる。 そのほうが要素数などの計算が簡単。 </p>
      *
      * @param l0 一次元目の要素数。
      * @param l1 二次元目の要素数。
@@ -42,16 +47,16 @@ public final class IntArray extends Array {
     public IntArray(final int l0, final int l1, final int l2, final int l3) {
 
         super(l0, l1, l2, l3);
-		int size=l0;
-		if(l1>0) {
-			size *= l1;
-			if(l2>0) {
-				size*=l2;
-				if(l3>0) {
-					size*=l3;
-				}
-			}
-		}
+        int size = l0;
+        if (l1 > 0) {
+            size *= l1;
+            if (l2 > 0) {
+                size *= l2;
+                if (l3 > 0) {
+                    size *= l3;
+                }
+            }
+        }
         values = new int[size];
     }
 
@@ -64,13 +69,13 @@ public final class IntArray extends Array {
     @Override
     public String toStringRaw(final int index) {
 
-        return Integer.toString(values[index]);
+        return Integer.toString(toIntRaw(index));
     }
 
     @Override
     public ByteString toByteStringRaw(final int index) {
 
-        return new ByteString(Integer.toString(values[index]));
+        return new ByteString(Integer.toString(toIntRaw(index)));
     }
 
     @Override
@@ -82,12 +87,12 @@ public final class IntArray extends Array {
     @Override
     public double toDoubleRaw(final int index) {
 
-        return values[index];
+        return toIntRaw(index);
     }
 
     @Override
     public Operand dupRaw(int index) {
-        return Scalar.fromValue(values[index]);
+        return Scalar.fromValue(toIntRaw(index));
     }
 
     @Override
@@ -102,165 +107,151 @@ public final class IntArray extends Array {
         --values[index];
     }
 
-    public void assignRaw(final int index, final int newValue){
+    public void assignRaw(final int index, final int newValue) {
         values[index] = newValue;
     }
-    public void assignRaw(final int index, final double newValue){
-        values[index] = (int)newValue;
+
+    public void assignRaw(final int index, final double newValue) {
+        assignRaw(index, (int) newValue);
     }
-    public void assignRaw(final int index, final String newValue){
-        values[index] = Conversion.strtoi(newValue);;
+
+    public void assignRaw(final int index, final String newValue) {
+        assignRaw(index, Conversion.strtoi(newValue));
     }
 
     @Override
     public void assignRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] = rhs.toIntRaw(rhi);
+        assignRaw(index, rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignAddRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] += rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) + rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignSubRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] -= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) - rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignMulRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] *= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) * rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignDivRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] /= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) / rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignModRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] %= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) % rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignAndRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] &= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) & rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignOrRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] |= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) | rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignXorRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] ^= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) ^ rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignSrRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] >>= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) >> rhs.toIntRaw(rhi));
     }
 
     @Override
     public void assignSlRaw(final int index, final Operand rhs, final int rhi) {
-        values[index] <<= rhs.toIntRaw(rhi);
+        assignRaw(index, toIntRaw(index) << rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand addRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] + rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) + rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand eqRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] == rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) == rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand neRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] != rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) != rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand gtRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] > rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) > rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand ltRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] < rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) < rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand geRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] >= rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) >= rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand leRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue((values[index] <= rhs.toIntRaw(rhi)) ? 1 : 0);
+        return Scalar.fromValue(toIntRaw(index) <= rhs.toIntRaw(rhi) ? 1 : 0);
     }
 
     @Override
     public Operand subRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] - rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) - rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand mulRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] * rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) * rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand divRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] / rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) / rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand modRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] % rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) % rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand andRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] & rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) & rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand orRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] | rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) | rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand xorRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] ^ rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) ^ rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand slRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] << rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) << rhs.toIntRaw(rhi));
     }
 
     @Override
     public Operand srRaw(final int index, final Operand rhs, final int rhi) {
-
-        return Scalar.fromValue(values[index] >> rhs.toIntRaw(rhi));
+        return Scalar.fromValue(toIntRaw(index) >> rhs.toIntRaw(rhi));
     }
 
     @Override
@@ -283,47 +274,47 @@ public final class IntArray extends Array {
 
     @Override
     public void assignNeRaw(int index, Operand rhs, int rhi) {
-        values[index] = (values[index] != rhs.toIntRaw(rhi)) ? 1 : 0;
+        assignRaw(index, (toIntRaw(index) != rhs.toIntRaw(rhi)) ? 1 : 0);
     }
 
     @Override
     public void assignGtRaw(int index, Operand rhs, int rhi) {
-        values[index] = (values[index] > rhs.toIntRaw(rhi)) ? 1 : 0;
+        assignRaw(index, (toIntRaw(index) > rhs.toIntRaw(rhi)) ? 1 : 0);
     }
 
     @Override
     public void assignLtRaw(int index, Operand rhs, int rhi) {
-        values[index] = (values[index] < rhs.toIntRaw(rhi)) ? 1 : 0;
+        assignRaw(index, (toIntRaw(index) < rhs.toIntRaw(rhi)) ? 1 : 0);
     }
 
     @Override
     public void assignGtEqRaw(int index, Operand rhs, int rhi) {
-        values[index] = (values[index] >= rhs.toIntRaw(rhi)) ? 1 : 0;
+        assignRaw(index, (toIntRaw(index) >= rhs.toIntRaw(rhi)) ? 1 : 0);
     }
 
     @Override
     public void assignLtEqRaw(int index, Operand rhs, int rhi) {
-        values[index] = (values[index] <= rhs.toIntRaw(rhi)) ? 1 : 0;
+        assignRaw(index, (toIntRaw(index) <= rhs.toIntRaw(rhi)) ? 1 : 0);
     }
 
-	//@Override
-	public void expandToIndexes() {
+    @Override
+    public void expandToIndexes() {
 
-		int size=l0;
-		if(l1>0) {
-			size *= l1;
-			if(l2>0) {
-				size*=l2;
-				if(l3>0) {
-					size*=l3;
-				}
-			}
-		}
+        int size = l0;
+        if (l1 > 0) {
+            size *= l1;
+            if (l2 > 0) {
+                size *= l2;
+                if (l3 > 0) {
+                    size *= l3;
+                }
+            }
+        }
 
-		final int[] newValues = new int[size];
+        final int[] newValues = new int[size];
 
-		System.arraycopy(values, 0, newValues, 0, values.length);
+        System.arraycopy(values, 0, newValues, 0, values.length);
 
-		values = newValues;
-	}
+        values = newValues;
+    }
 }
